2de9cdc97f66f675310c9aebb5d0a1debbdc5d16
Tajbir23 <studenttajbirislam@gmail.com>
2025-04-03 00:08:41 +0600

    test

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
