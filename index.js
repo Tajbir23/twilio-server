4e940e9b57b79a2498b38eab9e2a2bacac5b1d48
Tajbir23 <studenttajbirislam@gmail.com>
2025-04-03 00:03:32 +0600

    test

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/sms', (req, res) => {
+    const incomingMessage = req.body.Body;
+    const fromNumber = req.body.From;
+    
+    console.log(`Received SMS from ${fromNumber}: ${incomingMessage}`);
+
+    res.send('<Response></Response>');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
